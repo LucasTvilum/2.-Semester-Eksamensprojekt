fa8a8ea7dacd964a914e51ae223fa81ab9d3820a
LucasTvilum <lucastvilum@gmail.com>
2025-11-14 11:58:11 +0100

    Initial template

---
+++ ServerApp/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello from Main");
+        // Your startup code here
+    }
+}
