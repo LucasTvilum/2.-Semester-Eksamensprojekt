cd733ee1be70da2249569a85659facdac85fc694
albertarnason <albertingerslevarnason@gmail.com>
2025-11-19 11:25:39 +0100

    Tilføjet dårlig login side og fixet lokaler og annoncer

--- Core/Models/Lokale.cs
+++ Core/Models/Lokale.cs
@@ -5,10 +5,10 @@ namespace Core.Models
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
-        public string Name { get; set; }
-        public string Location { get; set; }
-        public string Åbningstid { get; set; }
-        public string Bemanding { get; set; }
+        public string Name { get; set; } = "";
+        public string Location { get; set; } = "";
+        public string Åbningstid { get; set; } = "";
+        public string Bemanding { get; set; } = "";
         public string Type { get; set; } = "normal";
         public string Adgang { get; set; } = "åben";
     }
