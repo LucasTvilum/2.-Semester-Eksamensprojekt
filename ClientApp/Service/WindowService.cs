ada886b03ae37d94f4c3a6bcd1bcee05233d3092
albertarnason <albertingerslevarnason@gmail.com>
2025-11-29 16:24:25 +0100

    Tilføjede add windows og showbookings

---
+++ ClientApp/Service/WindowService.cs
@@ -0,0 +1,6 @@
+namespace ClientApp.Service;
+
+public class WindowService
+{
+    
+}
---
+++ ServerApp/Repository/WindowMongoDB.cs
@@ -0,0 +1,39 @@
+using MongoDB.Driver;
+using Core.Models;
+using ServerApp.Repository;
+
+
+public class BookingMongoDB : IBookingRepository
+{
+    private readonly IMongoCollection<Booking> _collection;
+
+    public BookingMongoDB()
+    {
+        var client = new MongoClient("mongodb://localhost:27017");
+        var database = client.GetDatabase("Vinduespudsning");
+        _collection = database.GetCollection<Booking>("Bookings");
+    }
+
+    public List<Booking> GetAll()
+    {
+        return _collection.Find(_ => true).ToList();
+    }
+
+    public void Add(Booking booking)
+    {
+        _collection.InsertOne(booking);
+    }
+
+    public Booking Update(Booking booking)
+    {
+        var filter = Builders<Booking>.Filter.Eq(a => a.BookingId, booking.BookingId);
+        _collection.ReplaceOne(filter, booking);
+        return booking;
+    }
+
+    public void Delete(string id)
+    {
+        _collection.DeleteOne(id);
+    }
+
+}
