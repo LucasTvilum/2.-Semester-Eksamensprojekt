ffe4495f73c82146a71bccbd9975e98ca7629c37
1sra0mar <israaaomar22@gmail.com>
2025-12-18 23:54:34 +0100

    Doneee

--- Core/Models/Booking.cs
+++ Core/Models/Booking.cs
@@ -7,16 +7,15 @@ public class Booking
     public string Status { get; set; } = "";
     public decimal Price { get; set; }
     
+    public BookingType TypeBooking { get; set; } = BookingType.EnkeltBooking;
     public DateTime Date { get; set; } = DateTime.Now.Date; //hvis enkeltbooking så skal den bruges
     public string Day { get; set; } = DateTime.Now.ToString("dddd");
-
-    public List<Window> Windows { get; set; } = new List<Window>();
-    public BookingType TypeBooking { get; set; } = BookingType.EnkeltBooking;
     public string NotesCustomer { get; set; } = "";
-    public string NotesWindowCleaner { get; set; } = "";
     public bool InsideJob { get; set; }
     public int OutdoorInterval { get; set; }
     public int InsideInterval { get; set; }
+    public string NotesWindowCleaner { get; set; } = "";
+    public List<Window> Windows { get; set; } = new List<Window>();
     
     public enum BookingType {
         Abonnement,
